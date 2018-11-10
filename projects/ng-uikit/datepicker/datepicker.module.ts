f60a8b02448177ed225573032b8d474f936f4e30
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2018-11-10 13:13:34 +0530

    Update datepicker.module.ts

--- projects/ng-uikit/datepicker/datepicker.module.ts
+++ projects/ng-uikit/datepicker/datepicker.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DatepickerComponent } from './datepicker.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {DatepickerComponent} from './datepicker.component';
+import {DropdownModule} from '../dropdown/dropdown.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    DropdownModule
   ],
-  declarations: [DatepickerComponent]
+  declarations: [DatepickerComponent],
+  exports: [DatepickerComponent]
 })
-export class DatepickerModule { }
+export class DatepickerModule {
+}
