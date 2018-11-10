2c196bf288354bb54d75917cdbc5bb4c337cb985
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2018-11-10 13:13:54 +0530

    Update datepicker.component.ts

--- projects/ng-uikit/datepicker/datepicker.component.ts
+++ projects/ng-uikit/datepicker/datepicker.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datepicker.component.css']
 })
 export class DatepickerComponent implements OnInit {
-
+  items = [
+    {
+      url: 'a',
+      text: 'Action',
+      isDivider: true
+    },
+    {
+      url: 'a',
+      text: 'Another action'
+    },
+    {
+      url: 'a',
+      text: 'Something else here'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
