c541a0e273fe6a0c8c4bfb8bc8a8363043d93851
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2018-11-11 22:46:14 +0530

    Update carousel.component.ts

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -21,6 +21,33 @@ export class CarouselComponent implements OnInit {
       alt: 'Third slide'
     }
   ];
+  codes = [`import {CarouselModule} from 'ng-uikit'`,`export class CarouselComponent implements OnInit {
+
+    items = [
+      {
+        src: 'https://via.placeholder.com/600x400/367fa9/FFFFFF?auto=yes&bg=777&fg=555&text=First slide',
+        alt: 'First slide'
+      },
+      {
+        src: 'https://via.placeholder.com/600x400/dd4b39/FFFFFF?auto=yes&bg=777&fg=555&text=Second slide',
+        alt: 'Second slide'
+      },
+      {
+        src: 'https://via.placeholder.com/600x400/00a65a/FFFFFF?auto=yes&bg=777&fg=555&text=Third slide',
+        alt: 'Third slide'
+      }
+    ];
+    logEvent(e) {
+      console.log(e);
+    }
+  }
+  `, `<ui-carousel
+    [items]="items"
+    (afterNext)="logEvent($event)"
+    (afterPrev)="logEvent($event)">
+</ui-carousel>`];
+
+  language = 'js';
 
   constructor() {
   }
@@ -32,5 +59,6 @@ export class CarouselComponent implements OnInit {
   logEvent(e) {
     console.log(e);
   }
+
 
 }
