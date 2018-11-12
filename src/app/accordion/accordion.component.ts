19fcebb65ad73805d52a9c0e8646bc03492e43d4
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2018-11-12 15:22:51 +0530

    Update accordion.component.ts

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Codes} from '../shared/demo';
 
 @Component({
   selector: 'app-accordion',
@@ -6,8 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accordion.component.css']
 })
 export class AccordionComponent implements OnInit {
+  codes: Codes = {
+    import: `import {CarouselModule} from 'ng-uikit'`,
+    component: `export class Demo implements OnInit { }`,
+    html: `<ui-accordion></ui-accordion>`,
+    properties: [{
+      name: '',
+      type: '',
+      default: '',
+      description: ''
+    }],
+    events: [{
+      name: '',
+      parameters: '',
+      description: ''
+    }],
+    styling: [{
+      selector: '',
+      description: ''
+    }]
+  };
+  language = 'html';
+  title = 'Accordion';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
