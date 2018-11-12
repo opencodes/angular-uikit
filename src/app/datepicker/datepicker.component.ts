4bfcdba4fc3db91f03350d3f5c1847d04a71e4d7
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2018-11-12 18:38:48 +0530

    Update datepicker.component.ts

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Codes} from '../shared/demo';
 
 @Component({
   selector: 'app-datepicker',
@@ -6,13 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datepicker.component.css']
 })
 export class DatepickerComponent implements OnInit {
+  codes: Codes = {
+    import: `import {CarouselModule} from 'ng-uikit'`,
+    component: `export class Demo implements OnInit { }`,
+    html: `<ui-card></ui-card>`,
+    properties: [{
+      name: '',
+      type: '',
+      default: '',
+      description: ''
+    }],
+    events: [{
+      name: '',
+      parameters: '',
+      description: ''
+    }],
+    styling: [{
+      selector: '',
+      description: ''
+    }]
+  };
+  language = 'html';
+  title = 'Datepicker';
+  selectedDateFrom: any;
+  selectedDateTo: any;
+  interpolate = {
+    language: 'language interpolated'
+  };
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-  onSelect(e){
-    console.log(e);
+
+
+  onSelectFrom(e) {
+    this.selectedDateFrom = '' + JSON.stringify(e) + '';
+  }
+
+  onSelectTo(e) {
+    this.selectedDateTo = '' + JSON.stringify(e) + '';
   }
 
 }
