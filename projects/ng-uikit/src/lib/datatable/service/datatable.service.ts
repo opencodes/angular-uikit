d9446bd9266b3cbd39ac996ae303e11f6545dde3
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2018-11-26 18:41:28 +0530

    Update datatable.service.ts

--- projects/ng-uikit/src/lib/datatable/service/datatable.service.ts
+++ projects/ng-uikit/src/lib/datatable/service/datatable.service.ts
@@ -7,7 +7,13 @@ export class DatatableService {
 
   constructor() { }
 
-  getPageData(rows: any, size: number, number: number){
-
+  getPageData(rows, size, pageNum): Page {
+    const start = (pageNum - 1) * size;
+    const last = (start + size) > rows.length ? rows.length : start + size;
+    return {
+      startIndex: start,
+      rows: rows.slice(start, last),
+      lastIndex: last
+    };
   }
 }
