c64d793149f2ce5c1b47849bb858ae1c9da17e69
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2018-11-26 18:38:52 +0530

    Update pagination.component.ts

--- projects/ng-uikit/src/lib/datatable/pagination/pagination.component.ts
+++ projects/ng-uikit/src/lib/datatable/pagination/pagination.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {PagingService} from '../service/paging.service';
 
 @Component({
   selector: 'ui-pagination',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent implements OnInit {
+  @Input() totalCount: number;
+  @Input() size = 10;
+  @Output() pageChange = new EventEmitter();
+  private pageCount: number;
+  pages: any[];
+  private pageNum: number=1;
 
-  constructor() { }
+
+  constructor(private _pgs: PagingService) {
+  }
 
   ngOnInit() {
+    this._pgs.size = this.size;
+    this._pgs.totalCount = this.totalCount;
+    this.pageCount = this._pgs.getListOfPage(this.totalCount);
+    this.pages = Array(this.pageCount || 0);
+    console.log(this.pages);
+  }
+
+  onPageChange(e) {
+    console.log(e);
+    this.pageNum = e;
+    this.pageChange.emit({
+      pageNum: e
+    });
   }
 
 }
