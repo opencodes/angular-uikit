6bd6657c487af9e6a8d30631fb754cebda9efab5
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2019-01-03 17:38:31 +0530

    Update datatable.component.ts

--- projects/ng-uikit/src/lib/datatable/datatable.component.ts
+++ projects/ng-uikit/src/lib/datatable/datatable.component.ts
@@ -8,18 +8,21 @@ import {DatatableService} from './service/datatable.service';
   styleUrls: ['./datatable.component.css']
 })
 export class DatatableComponent implements OnInit {
+
   @Input() rows;
   @Input() columns: Column;
   @Output() pageChange = new EventEmitter();
   @Output() sorted = new EventEmitter();
-  @Input() size = 6;
+  size = 6;
   page: Page;
+  search: any = {};
 
   constructor(private _dts: DatatableService) {
   }
 
   ngOnInit() {
     this.page = this._dts.getPageData(this.rows, this.size, 1);
+    console.log(this.page);
   }
 
   getColumnClass(col: Column) {
@@ -36,5 +39,49 @@ export class DatatableComponent implements OnInit {
 
   onPageChange(e: Paging) {
     this.page = this._dts.getPageData(this.rows, this.size, e.pageNum);
+  }
+
+  sort(item) {
+    this.page.rows.sort(this.dynamicSort(item.field));
+  }
+
+  dynamicSort(property) {
+    var sortOrder = 1;
+    if (property[0] === '-') {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+    return function (a, b) {
+      if (sortOrder === -1) {
+        return b[property].toString().localeCompare(a[property]);
+      } else {
+        return a[property].toString().localeCompare(b[property]);
+      }
+    };
+  }
+
+  filter(event, field, val) {
+    if (val.toString().length) {
+      if (event.keyCode === 13 || event.which === 13) {
+        this.search = {};
+        this.search[field] = val;
+        console.log(field, this.search, this.rows);
+        const rows = [];
+        this.rows.forEach((item) => {
+          if (item[field].toString().toLowerCase().indexOf(val.toString().toLowerCase()) !== -1) {
+            rows.push(item);
+          }
+        });
+        this.page = this._dts.getPageData(rows, this.size, 1);
+      }
+    } else {
+      this.page = this._dts.getPageData(this.rows, this.size, 1);
+    }
+  }
+
+  changeFilter(val) {
+    if (val) {
+
+    }
   }
 }
